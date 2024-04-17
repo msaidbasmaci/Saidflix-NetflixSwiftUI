faf3a7550e5736fd4ff75e94c4fe733e4f250fa5
msaidbasmaci <muhammedbsmcm@gmail.com>
2024-04-17 15:58:46 +0300

    Initial Commit

---
+++ Saidflix/SaidflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaidflixApp.swift
+//  Saidflix
+//
+//  Created by Muhammed Said BASMACI on 17.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SaidflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
